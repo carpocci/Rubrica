601ff484144df24490e7a250a61e42f379f3ed67
Andrea Carpino <carpocci@gmail.com>
2023-09-27 10:48:48 +0200

    Esercizio 1

---
+++ Rubrica/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+
+StreamReader r = new("rubrica.json");
+string? json = r.ReadLine();
+if (json == null)
+{
+    Console.WriteLine("Error");
+    return;
+}
+
+Contatto? contatto = JsonSerializer.Deserialize<Contatto>(json);
+
+if (contatto == null)
+{
+    Console.WriteLine("Error");
+    return;
+}
+
+Console.WriteLine($"{contatto.Nome} {contatto.Cognome}: {contatto.Numero}");
+
+public class Contatto
+{
+    public string Nome { get; set; } = "";
+    public string Cognome { get; set; } = "";
+    public string Numero { get; set; } = "";
+}
